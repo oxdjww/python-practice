bef7d27c67eacf6711283627725a091dd109ea41
oxdjww <oxdjww@gmail.com>
2023-09-17 20:11:42 +0900

    [+] add chapter 08,09,10

---
+++ Chapter08/range.py
@@ -0,0 +1,15 @@
+for i in range(60):
+    print(i+1 ,"분")
+
+for i in range(12):
+    print(i+1,"월")
+
+for i in range(1,11):
+    print(i, "번째 페이지입니다.")
+
+count = 0
+while count<5:
+    print(count,"번째 반복문입니다.")
+    count +=1
+
+
---
+++ Chapter09/ex1.py
@@ -0,0 +1,8 @@
+input = int(input("숫자 입력>>"))
+
+sum = 0
+
+for i in range(0, input+1):
+    sum += i
+
+print(sum)
